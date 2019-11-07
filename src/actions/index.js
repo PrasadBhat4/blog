1b60d09cc00b1b5dd4148d6f8583240871038443
PrasadBhat4 <40450109+PrasadBhat4@users.noreply.github.com>
2019-11-07 16:19:44 +0530

    updated commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,19 @@
+import _ from "lodash";
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  //alternate method using lodash chain
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceHolder.get("/posts");
@@ -7,16 +22,16 @@ export const fetchPosts = () => async dispatch => {
 };
 
 export const fetchUser = id => async dispatch => {
-  const response = await jsonPlaceHolder.get(`/users/ ${id}`);
+  const response = await jsonPlaceHolder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-export const fetchPosts = () => {
-  return function(dispatch, getState) {
-    const promise = jsonPlaceHolder.get("/posts");
-    return {
-      type: "FETCH_POSTS",
-      payload: promise
-    };
-  };
 };
-}
+
+//memoized solution
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
